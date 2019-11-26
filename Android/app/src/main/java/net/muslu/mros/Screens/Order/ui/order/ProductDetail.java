7abbd00467bec93f2464e13fc0a850e1c2268a39
Tayyip MUSLU <muhammet.muslu@ceng.deu.edu.tr>
2019-11-26 16:24:10 +0300

    product detail activity

---
+++ Android/app/src/main/java/net/muslu/mros/Screens/Order/ui/order/ProductDetail.java
@@ -0,0 +1,14 @@
+package net.muslu.mros.Screens.Order.ui.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import net.muslu.mros.R;
+
+public class ProductDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
