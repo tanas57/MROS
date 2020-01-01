2c7a1695d474638a13b44a5b91c265de35b06101
Tayyip MUSLU <muhammet.muslu@ceng.deu.edu.tr>
2020-01-01 02:16:19 -0800

    initial ios

---
+++ iOS/MROS/MROS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MROS
+//
+//  Created by Tayyip Muslu on 1.01.2020.
+//  Copyright © 2020 Tayyip Muslu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
